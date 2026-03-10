ec5767d0d73cda311fae050ab14a36111b62293c
Sergio Lugo Couros <alu0101423579@ull.edu.es>
2026-03-10 09:45:23 +0000

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+project = 'Interfaz Gráfica para Control de Maqueta de Temperatura'
+author = 'Sergio Lugo'
+release = '1.0'
+
+extensions = [
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
